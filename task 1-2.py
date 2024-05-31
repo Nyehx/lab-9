0f8b1d89a0a0dc5556e03fa581c5c02251f4910e
Nyehx <144653367+Nyehx@users.noreply.github.com>
2024-05-31 15:41:45 +0300

    Add files via upload

--- task 1-2.py
+++ task 1-2.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+def counting_sort_strings(arr, temp, ascending=True):
+    count = {s: 0 for s in temp}
+    for s in arr:
+        count[s] += 1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    new_arr = []
+    if ascending:
+        for s in temp:
+            new_arr.extend([s] * count[s])
+    else:
+        for s in reversed(temp):
+            new_arr.extend([s] * count[s])
 
+    return new_arr
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def test():
+    # Тест 1: Массив строк разных длин
+    template1 = ["One", "Two", "Three", "Four"]
+    arr1 = ["One", "Three", "Three", "Three", "Two"]
+    assert counting_sort_strings(arr1, template1) == ["One", "Two", "Three", "Three", "Three"]
+    print("Тест 1 пройден")
 
+    # Тест 2: Строки на разных языках
+    template2 = ["Один", "Два", "Three", "Four"]
+    arr2 = ["Three", "Four", "Один", "Два", "Three"]
+    assert counting_sort_strings(arr2, template2) == ["Один", "Два", "Three", "Three", "Four"]
+    print("Тест 2 пройден")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Тест 3: Пустой массив
+    template3 = ["Один", "Два", "Три"]
+    arr3 = []
+    assert counting_sort_strings(arr3, template3) == []
+    print("Тест 3 пройден")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Все тесты пройдены")
+
+test()
